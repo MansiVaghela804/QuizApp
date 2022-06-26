16bf1987c4ee8c099da92c0dd53e23ea6f59b66b
Mansi <mansivaghela804@gmail.com>
2022-06-26 12:27:33 +0200

    Commit.

--- app/src/main/java/com/mandev/quizapp/MainActivity.kt
+++ app/src/main/java/com/mandev/quizapp/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.mandev.quizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
